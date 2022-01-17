477994fd0b6adcd4f4429dceb9cbe2f187a32db0
ArthurPV <arthurpontiervalois@gmail.com>
2022-01-16 22:08:31 -0500

    Move to compile.ml and rename type

--- src/core/bytecode/vm.mli
+++ src/core/bytecode/vm.mli
@@ -1,31 +1,14 @@
 open Opcode
-
-module VMValue : sig
-    type t =
-      | Int of Stdint.int64 [@printer fun fmt i -> fprintf fmt "Int(%s)" (Stdint.Int64.to_string i)]
-      | Float of float
-      | Bool of bool
-      | String of string
-      | Char of char
-      | Undef
-      | Nil
-    [@@deriving show]
-
-    val to_int : t -> Stdint.int64
-    val to_float : t -> float
-    val to_bool : t -> bool
-    val to_string : t -> string
-    val to_char : t -> char
-end
+open Compile
 
 module Chunk : sig
   type t = {
     code : opcode array;
-    constants : VMValue.t array;
+    constants : LIR.t array; (* change that *)
   }
 
   val new_chunk : t
-  val get_constant : t -> int -> VMValue.t
+  val get_constant : t -> int -> LIR.t
 end
 
 val stack_size : int
@@ -33,5 +16,5 @@ val stack_size : int
 type vm = {
   chunk : Chunk.t;
   ip : Stdint.uint8;
-  stack : VMValue.t array;
+  stack : LIR.t array; (* change that *)
 }
