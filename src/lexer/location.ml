8764eecee0b5a502afacdf6cda29ef8ad3a102aa
ArthurPV <arthurpontiervalois@gmail.com>
2022-01-18 02:04:12 -0500

    Add [@@deriving show] on location type

--- src/lexer/location.ml
+++ src/lexer/location.ml
@@ -1,23 +1,25 @@
 type location = {
-  mutable line : int;
-  mutable col : int;
-  mutable s_line : int;
-  mutable e_line : int;
-  mutable s_col : int;
-  mutable e_col : int;
+  mutable line : int; [@printer fun fmt l -> fprintf fmt "%d" l]
+  mutable col : int; [@printer fun fmt c -> fprintf fmt "%d" c]
+  mutable s_line : int; [@printer fun fmt sl -> fprintf fmt "%d" sl]
+  mutable e_line : int; [@printer fun fmt el -> fprintf fmt "%d" el]
+  mutable s_col : int; [@printer fun fmt sc -> fprintf fmt "%d" sc]
+  mutable e_col : int; [@printer fun fmt ec -> fprintf fmt "%d" ec]
 }
+[@@deriving show]
 
 let new_location =
   { line = 1; col = 1; s_line = 1; e_line = 1; s_col = 1; e_col = 1 }
 
-let copy_location loc = {
+let copy_location loc =
+  {
     line = loc.line;
     col = loc.col;
     s_line = loc.s_line;
     s_col = loc.s_col;
     e_line = loc.e_line;
     e_col = loc.e_col;
-}
+  }
 
 let end_location loc loc2 =
   loc.e_line <- loc2.e_line;
--- src/lexer/location.mli
+++ src/lexer/location.mli
@@ -1,11 +1,12 @@
 type location = {
-  mutable line : int;
-  mutable col : int;
-  mutable s_line : int;
-  mutable e_line : int;
-  mutable s_col : int;
-  mutable e_col : int;
+  mutable line : int; [@printer fun fmt l -> fprintf fmt "%d" l]
+  mutable col : int; [@printer fun fmt c -> fprintf fmt "%d" c]
+  mutable s_line : int; [@printer fun fmt sl -> fprintf fmt "%d" sl]
+  mutable e_line : int; [@printer fun fmt el -> fprintf fmt "%d" el]
+  mutable s_col : int; [@printer fun fmt sc -> fprintf fmt "%d" sc]
+  mutable e_col : int; [@printer fun fmt ec -> fprintf fmt "%d" ec]
 }
+[@@deriving show]
 
 val new_location : location
 val copy_location : location -> location
