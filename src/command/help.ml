48b4ae81b146d997cf28f77809a0d9c657fc213a
ArthurPV <arthurpontiervalois@gmail.com>
2022-02-01 22:29:33 -0500

    Close Make root help #3

--- src/command/help.ml
+++ src/command/help.ml
@@ -1,2 +1,23 @@
-let main_help = "Help"
+let main_help =
+  {|Usage: lily [command] [options]
+
+Options:
+
+    -h, --help          Print lily's help
+    -v, --version       Print lily's version
+
+Commands:
+
+    build               Build project
+    compile             Compile file
+    help                Print lily's help
+    init                Init project
+    new                 New project
+    repl                Start REPL
+    run                 Run file (bytecode)
+    test                Test file
+    to                  Transpile file in Js, Ml, Rb,...
+    version             Print lily's version
+|}
+
 let run_help = "Help"
