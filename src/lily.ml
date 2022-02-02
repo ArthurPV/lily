babe1d7c96ed49735c36a7a7741d8258e30a5015
ArthurPV <arthurpontiervalois@gmail.com>
2022-02-01 22:45:45 -0500

    Update main file

--- src/lily.ml
+++ src/lily.ml
@@ -1,7 +1,6 @@
-module Run = Lily_command.Run
-open Argument
+open Lily_cli.Argument
+open Lily_cli.Commands
 
 let () =
   let arg = new_argument in
-  if arg.argc > 1 then Run.run_bytecode arg.argv.(2)
-  else Printf.printf "Hello"
+  parse |> run arg
