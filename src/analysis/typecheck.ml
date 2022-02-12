12eb917fbdad798863914980c29cdfaa9b1b0fb4
ArthurPV <arthurpontiervalois@gmail.com>
2022-02-12 10:53:55 -0500

    Update analysis

--- src/analysis/typecheck.ml
+++ src/analysis/typecheck.ml
@@ -9,7 +9,8 @@ type typecheck = Parser.parser
 let rec can_check_type node = assert false
 and check_generics_type tpc node = assert false
 
-and check_binary_expr tpc = function
+and check_arithmetic_expr dts =
+  match dts with
   | `I8, `I8 -> `I8
   | `I16, `I16 -> `I16
   | `I32, `I32 -> `I32
@@ -18,106 +19,65 @@ and check_binary_expr tpc = function
   | `U16, `U16 -> `U16
   | `U32, `U32 -> `U32
   | `U64, `U64 -> `U64
-  | `I8, dt ->
+  | `F32, `F32 -> `F32
+  | `F64, `F64 -> `F64
+  | `I8, dt
+  | `I16, dt
+  | `I32, dt
+  | `I64, dt
+  | `U8, dt
+  | `U16, dt
+  | `U32, dt
+  | `U64, dt
+  | `F32, dt
+  | `F64, dt ->
       dt |> show_data_type
       |> Printf.sprintf
-           "the expression on the left has type Int8, but the type on the \
+           "the expression on the left has type %s, but the type on the \
             right has type %s"
+           (match dts with t, _ -> t |> show_data_type)
       |> failwith
-  | `I16, dt ->
+  | dt, `I8
+  | dt, `I16
+  | dt, `I32
+  | dt, `I64
+  | dt, `U8
+  | dt, `U16
+  | dt, `U32
+  | dt, `U64
+  | dt, `F32
+  | dt, `F64 ->
       dt |> show_data_type
       |> Printf.sprintf
-           "the expression on the left has type Int16, but the type on the \
-            right has type %s"
-      |> failwith
-  | `I32, dt ->
-      dt |> show_data_type
-      |> Printf.sprintf
-           "the expression on the left has type Int32, but the type on the \
-            right type %s"
-      |> failwith
-  | `I64, dt ->
-      dt |> show_data_type
-      |> Printf.sprintf
-           "the expression on the left has type Int64, but the type on the \
-            right type %s"
-      |> failwith
-  | `U8, dt ->
-      dt |> show_data_type
-      |> Printf.sprintf
-           "the expression on the left, has type Uint8, but the type on the \
-            right type %s"
-      |> failwith
-  | `U16, dt ->
-      dt |> show_data_type
-      |> Printf.sprintf
-           "the expression on the left, has type Uint16, but the type on \
-            the right type %s"
-      |> failwith
-  | `U32, dt ->
-      dt |> show_data_type
-      |> Printf.sprintf
-           "the expression on the left, has type Uint32, but the type on \
-            the right type %s"
-      |> failwith
-  | `U64, dt ->
-      dt |> show_data_type
-      |> Printf.sprintf
-           "the expression on the left, has type Uint64, but the type on \
-            the right type %s"
-      |> failwith
-  | dt, `I8 ->
-      dt |> show_data_type
-      |> Printf.sprintf
-           "the expression on the right, has type Int8, but the type on the \
-            left type %s"
-      |> failwith
-  | dt, `I16 ->
-      dt |> show_data_type
-      |> Printf.sprintf
-           "the expression on the right, has type Int16, but the type on \
-            the left type %s"
-      |> failwith
-  | dt, `I32 ->
-      dt |> show_data_type
-      |> Printf.sprintf
-           "the expression on the right, has type Int32, but the type on \
-            the left type %s"
-      |> failwith
-  | dt, `I64 ->
-      dt |> show_data_type
-      |> Printf.sprintf
-           "the expression on the right, has type Int64, but the type on \
-            the left type %s"
-      |> failwith
-  | dt, `U8 ->
-      dt |> show_data_type
-      |> Printf.sprintf
-           "the expression on the right, has type Uint8, but the type on \
-            the left type %s"
-      |> failwith
-  | dt, `U16 ->
-      dt |> show_data_type
-      |> Printf.sprintf
-           "the expression on the right, has type Uint16, but the type on \
-            the left type %s"
-      |> failwith
-  | dt, `U32 ->
-      dt |> show_data_type
-      |> Printf.sprintf
-           "the expression on the right, has type Uint32, but the type on \
-            the left type %s"
-      |> failwith
-  | dt, `U64 ->
-      dt |> show_data_type
-      |> Printf.sprintf
-           "the expression on the right, has type Uint64, but the type on \
-            the left type %s"
+           "the expression on the right, has type %s, but the type on the \
+            left has type %s"
+           (match dts with _, t -> t |> show_data_type)
       |> failwith
   | dt, dt2 ->
       dt |> show_data_type
       |> Printf.sprintf
-           "bad type for binary expression: [left: %s, right: %s]"
+           "bad type for arithmetic expression: [left: %s, right: %s]"
+           (dt2 |> show_data_type)
+      |> failwith
+
+and check_logical_expr = function
+  | `Bool, `Bool -> `Bool
+  | `Bool, dt ->
+      dt |> show_data_type
+      |> Printf.sprintf
+           "the expression on the left has type Bool, but the type on the \
+            right has type %s"
+      |> failwith
+  | dt, `Bool ->
+      dt |> show_data_type
+      |> Printf.sprintf
+           "the expression on the right has type Bool, but the type on the \
+            left has type %s"
+      |> failwith
+  | dt, dt2 ->
+      dt |> show_data_type
+      |> Printf.sprintf
+           "bad type for logical expression: [left: %s, right: %s]"
            (dt2 |> show_data_type)
       |> failwith
 
@@ -126,24 +86,90 @@ and check_expr_type tpc = function
   | Positive x, l -> check_expr_type tpc (x, l)
   | Negative x, l -> check_expr_type tpc (x, l)
   | Not x, l -> check_expr_type tpc (x, l)
-  | Mul (x, y), l ->
+  | Mul (x, y), l
+  | Div (x, y), l
+  | Mod (x, y), l
+  | Add (x, y), l
+  | Sub (x, y), l
+  | Exp (x, y), l ->
       let left = check_expr_type tpc (x, l) in
       let right = check_expr_type tpc (y, l) in
-      check_binary_expr tpc (left, right)
-  | Div (x, y), _ -> failwith "not implemented"
-  | Mod (x, y), _ -> failwith "not implemented"
-  | Add (x, y), _ -> failwith "not implemented"
-  | Sub (x, y), _ -> failwith "not implemented"
-  | Exp (x, y), _ -> failwith "not implemented"
-  | Range (x, y), _ -> failwith "not implemented"
-  | Lt (x, y), _ -> failwith "not implemented"
-  | Gt (x, y), _ -> failwith "not implemented"
-  | Le (x, y), _ -> failwith "not implemented"
-  | Ge (x, y), _ -> failwith "not implemented"
+      (* TODO: add try *)
+      check_arithmetic_expr (left, right)
+  | Range (x, y), l -> (
+      let left = check_expr_type tpc (x, l) in
+      let right = check_expr_type tpc (y, l) in
+      match (left, right) with
+      | `I8, `I8 -> `I8
+      | `I16, `I16 -> `I16
+      | `I32, `I32 -> `I32
+      | `I64, `I64 -> `I64
+      | `U8, `U8 -> `U8
+      | `U16, `U16 -> `U16
+      | `U32, `U32 -> `U32
+      | `U64, `U64 -> `U64
+      | `Char, `Char -> `Char
+      | `I8, dt
+      | `I16, dt
+      | `I32, dt
+      | `I64, dt
+      | `U8, dt
+      | `U16, dt
+      | `U32, dt
+      | `U64, dt ->
+          dt |> show_data_type
+          |> Printf.sprintf
+               "the expression on the right, has type %s, but the type on \
+                the left has type %s"
+               (left |> show_data_type)
+          |> failwith
+      | dt, `I8
+      | dt, `I16
+      | dt, `I32
+      | dt, `I64
+      | dt, `U8
+      | dt, `U16
+      | dt, `U32
+      | dt, `U64 ->
+          dt |> show_data_type
+          |> Printf.sprintf
+               "the expression on the left, has type %s, but the type on \
+                the right has type %s"
+               (right |> show_data_type)
+          |> failwith
+      | dt, dt2 ->
+          dt |> show_data_type
+          |> Printf.sprintf
+               "bad type for range expression: [left: %s, right: %s]"
+               (dt2 |> show_data_type)
+          |> failwith)
+  | Lt (x, y), l
+  | Gt (x, y), l
+  | Le (x, y), l
+  | Ge (x, y), l ->
+      let left = check_expr_type tpc (x, l) in
+      let right = check_expr_type tpc (y, l) in
+      (match (left, right) with
+      | `I8, `I8 -> `Bool
+      | `I16, `I16 -> `Bool
+      | `I32, `I32 -> `Bool
+      | `I64, `I64 -> `Bool
+      | `U8, `U8 -> `Bool
+      | `U16, `U16 -> `Bool
+      | `U32, `U32 -> `Bool
+      | `U64, `U64 -> `Bool
+      | `F32, `F32 -> `Bool
+      | `F64, `F64 -> `Bool
+      | _ -> failwith "error")
   | Eq (x, y), _ -> failwith "not implemented"
   | Ne (x, y), _ -> failwith "not implemented"
-  | And (x, y), _ -> failwith "not implemented"
-  | Or (x, y), _ -> failwith "not implemented"
+  | And (x, y), l
+  | Or (x, y), l ->
+      let left = check_expr_type tpc (x, l) in
+      let right = check_expr_type tpc (y, l) in
+      (match (left, right) with
+      | `Bool, `Bool -> `Bool
+      | _ -> failwith "error")
   | Assign (x, y), _ -> failwith "not implemented"
   | AddAssign (x, y), _ -> failwith "not implemented"
   | SubAssign (x, y), _ -> failwith "not implemented"
--- src/analysis/typecheck.mli
+++ src/analysis/typecheck.mli
@@ -6,6 +6,7 @@ type typecheck = parser
 
 val can_check_type : ast * location -> bool
 val check_generics_type : typecheck -> ast * location -> data_type
-val check_binary_expr : typecheck -> data_type * data_type -> data_type
+val check_arithmetic_expr : data_type * data_type -> data_type
+val check_logical_expr : data_type * data_type -> data_type
 val check_expr_type : typecheck -> expr * location -> data_type
 val check_type : typecheck -> ast * location -> data_type
