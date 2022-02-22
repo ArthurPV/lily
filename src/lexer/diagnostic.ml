4ff8855dd759b358e351d96ca018d07a3b5cc5a8
ArthurPV <arthurpontiervalois@gmail.com>
2022-02-21 19:10:02 -0500

    Add colors output

--- src/lexer/diagnostic.ml
+++ src/lexer/diagnostic.ml
@@ -1,10 +1,10 @@
 open Location
 
 type diagnostic_kind =
-  | Error [@printer fun fmt _ -> fprintf fmt "error"]
-  | Warning [@printer fun fmt _ -> fprintf fmt "warning"]
-  | Note [@printer fun fmt _ -> fprintf fmt "note"]
-  | Internal [@printer fun fmt _ -> fprintf fmt "internal"]
+  | Error [@printer fun fmt _ -> fprintf fmt "\x1b[1m\x1b[31merror\x1b[0m\x1b[0m"]
+  | Warning [@printer fun fmt _ -> fprintf fmt "\x1b[1m\x1b[33mwarning\x1b[0m\x1b[0m"]
+  | Note [@printer fun fmt _ -> fprintf fmt "\x1b[1m\x1b[36mnote\x1b[0m\x1b[0m"]
+  | Internal [@printer fun fmt _ -> fprintf fmt "\x1b[1minternal\x1b[0m"]
 [@@deriving show]
 
 exception EmitDiagnostic of string * diagnostic_kind * location
@@ -19,7 +19,7 @@ type diagnostic = {
 let new_diagnostic ~msg kind loc ~filename = { msg; kind; loc; filename }
 
 let diagnostic_to_string dgn =
-  Printf.sprintf "%s:%d:%d - %s: %s\n" dgn.filename dgn.loc.line dgn.loc.col
+  Printf.sprintf "\x1b[1m%s:%d:%d - %s: %s\x1b[0m\n" dgn.filename dgn.loc.line dgn.loc.col
     (show_diagnostic_kind dgn.kind)
     dgn.msg
 
