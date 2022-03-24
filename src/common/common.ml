c47431c8006d8d5f669c008e813ee7c1534e9904
ArthurPV <arthurpontiervalois@gmail.com>
2022-03-23 21:11:58 -0400

    Update

--- src/common/common.ml
+++ src/common/common.ml
@@ -1,3 +1,12 @@
 let rec list_insert v i = function
   | [] -> [ v ]
   | z :: le as l -> if i = 0 then v :: l else z :: list_insert v (i - 1) le
+
+let rec list_remove n = function
+  | [] -> []
+  | h :: t -> if n = 0 then t else h :: list_remove (n - 1) t
+
+let list_remove_last = function
+  | [] -> []
+  | t ->
+      Array.sub (t |> Array.of_list) 0 (List.length t - 1) |> Array.to_list
--- src/common/common.mli
+++ src/common/common.mli
@@ -1,1 +1,3 @@
 val list_insert : 'a -> int -> 'a list -> 'a list
+val list_remove : int -> 'a list -> 'a list
+val list_remove_last : 'a list -> 'a list
