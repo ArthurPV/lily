88626c8d7d9791cccda413f4a48b2e0f980376cd
ArthurPV <arthurpontiervalois@gmail.com>
2022-03-26 18:13:35 -0400

    Add documentation

--- src/lexer/source.mli
+++ src/lexer/source.mli
@@ -2,11 +2,17 @@ open Lily_common.Error
 
 type source = {
   filename : string;
-  content : string;
+  content : string; (* Content of filename *)
   mutable c : char;
-  len : int;
-  mutable pos : int;
+      (* A character from the position in the content (content.[pos]) *)
+  len : int; (* Length of content *)
+  mutable pos : int; (* Position in content; default: 0 *)
 }
 
 val new_source : string -> string -> source
+(** [new_source filename content] Init source type *)
+
 val read_file : string -> (string, cli_error_kind) result
+(** [read_file filename] Read file from filename. Return Error cli_error_kind
+    if the [filename] is not found or if is a directory or if has a bad
+    extension else return Ok content. *)
