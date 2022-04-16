b0b16c402bcb5705208d9015dd1efc0c327b4bdd
ArthurPV <arthurpontiervalois@gmail.com>
2022-04-16 13:23:15 -0400

    Create mod function for int32, int64 and int128 types

---
+++ src/common/mod.ml
@@ -0,0 +1,29 @@
+module Int32 = struct
+  let ( mod ) x y =
+    let rec loop ?(i = Stdint.Int32.of_int 0) () =
+      if Stdint.Int32.( * ) i y < x then
+        loop ~i:(Stdint.Int32.of_int 1 |> Stdint.Int32.( + ) i) ()
+      else Stdint.Int32.( * ) i y
+    in
+    loop ()
+end
+
+module Int64 = struct
+  let ( mod ) x y =
+    let rec loop ?(i = Stdint.Int64.of_int 0) () =
+      if Stdint.Int64.( * ) i y < x then
+        loop ~i:(Stdint.Int64.of_int 1 |> Stdint.Int64.( + ) i) ()
+      else Stdint.Int64.( * ) i y
+    in
+    loop ()
+end
+
+module Int128 = struct
+  let ( mod ) x y =
+    let rec loop ?(i = Stdint.Int128.of_int 0) () =
+      if Stdint.Int128.( * ) i y < x then
+        loop ~i:(Stdint.Int128.of_int 1 |> Stdint.Int128.( + ) i) ()
+      else Stdint.Int128.( * ) i y
+    in
+    loop ()
+end
---
+++ src/common/mod.mli
@@ -0,0 +1,11 @@
+module Int32 : sig
+  val ( mod ) : Stdint.Int32.t -> Stdint.Int32.t -> Stdint.Int32.t
+end
+
+module Int64 : sig
+  val ( mod ) : Stdint.Int64.t -> Stdint.Int64.t -> Stdint.Int64.t
+end
+
+module Int128 : sig
+  val ( mod ) : Stdint.Int128.t -> Stdint.Int128.t -> Stdint.Int128.t
+end
