eec05154bcfde071da15ec9c6d7689149e18bd32
ArthurPV <arthurpontiervalois@gmail.com>
2022-04-16 14:31:07 -0400

    Update

--- src/common/mod.ml
+++ src/common/mod.ml
@@ -1,7 +1,7 @@
 module Int32 = struct
   let ( mod ) x y =
     let rec loop ?(i = Stdint.Int32.of_int 0) () =
-      if Stdint.Int32.( * ) i y < x then
+      if Stdint.Int32.( * ) i y <= x then
         loop ~i:(Stdint.Int32.of_int 1 |> Stdint.Int32.( + ) i) ()
       else Stdint.Int32.( * ) i y
     in
@@ -11,7 +11,7 @@ end
 module Int64 = struct
   let ( mod ) x y =
     let rec loop ?(i = Stdint.Int64.of_int 0) () =
-      if Stdint.Int64.( * ) i y < x then
+      if Stdint.Int64.( * ) i y <= x then
         loop ~i:(Stdint.Int64.of_int 1 |> Stdint.Int64.( + ) i) ()
       else Stdint.Int64.( * ) i y
     in
@@ -21,7 +21,7 @@ end
 module Int128 = struct
   let ( mod ) x y =
     let rec loop ?(i = Stdint.Int128.of_int 0) () =
-      if Stdint.Int128.( * ) i y < x then
+      if Stdint.Int128.( * ) i y <= x then
         loop ~i:(Stdint.Int128.of_int 1 |> Stdint.Int128.( + ) i) ()
       else Stdint.Int128.( * ) i y
     in
